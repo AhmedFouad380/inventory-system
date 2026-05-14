886d5dd6c3dbe670cae71e3d9e6725d10299935e
AhmedFouad380 <36769581+AhmedFouad380@users.noreply.github.com>
2026-05-14 16:32:39 +0300

    a

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,11 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
 
 #[Fillable(['name', 'email', 'password', 'is_active'])]
 #[Hidden(['password', 'remember_token'])]
